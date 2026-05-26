305e3569e89d076707672550dd3beb54c842a72a
ShyamDodway <shyamdodway058@gmail.com>
2026-05-26 13:03:42 +0530

    Saved local changes on main

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import os
 from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_groq import ChatGroq
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda
@@ -22,13 +23,18 @@ def create_vector_store(transcript: str):
     """Split transcript and create FAISS vector store."""
     splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     chunks = splitter.create_documents([transcript])
-    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+    embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
     vector_store = FAISS.from_documents(chunks, embeddings)
     return vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 4})
 
 def build_rag_chain(retriever):
     """Create the LangChain pipeline."""
-    llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.2)
+    llm = ChatGroq(
+    model_name="llama-3.1-8b-instant",
+    temperature=0.2
+)
     prompt = PromptTemplate(
         template="""
         You are a helpful assistant.
